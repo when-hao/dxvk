33285d25c5a6077bd5e41a88fa4af2fbee5d7f28
when-hao <72253870+when-hao@users.noreply.github.com>
2024-12-02 01:12:14 +0800

    Update package-release.sh

--- package-release.sh
+++ package-release.sh
@@ -73,12 +73,10 @@ function build_arch {
         $opt_strip                                          \
         --bindir "x$1"                                      \
         --libdir "x$1"                                      \
-        -Db_lto=true \
-        -Db_pgo=generate \
-        -Dc_args="-fprofile-arcs -ftest-coverage" \
-        -Dc_link_args=" -fprofile-arcs -ftest-coverage -lgcov" \
-        -Dcpp_args="-fprofile-arcs -ftest-coverage" \
-        -Dcpp_link_args="-fprofile-arcs -ftest-coverage -lgcov" \
+        -Dc_args="-finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -fgraphite-identity -floop-nest-optimize -ftree-loop-distribution -fno-semantic-interposition -fipa-pta -fno-plt" \
+        -Dc_link_args="-flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
+        -Dcpp_args="-finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -fgraphite-identity -floop-nest-optimize -ftree-loop-distribution -fno-semantic-interposition -fipa-pta -fno-plt" \
+        -Dcpp_link_args="-flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
         -Db_ndebug=if-release                               \
         -Dbuild_id=$opt_buildid                             \
         "$DXVK_BUILD_DIR/build.$1"
