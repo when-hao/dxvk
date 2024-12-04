18335f4b0e73dd179b497a6735b6fd1ddcee5c5c
when-hao <72253870+when-hao@users.noreply.github.com>
2024-12-04 17:10:53 +0800

    Update package-release.sh

--- package-release.sh
+++ package-release.sh
@@ -74,7 +74,7 @@ function build_arch {
         --bindir "x$1"                                      \
         --libdir "x$1" \
         -Db_pgo=off \
-        -Db_sanitize=none
+        -Db_sanitize=none \
         -Dc_args="-finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -fgraphite-identity -floop-nest-optimize -ftree-loop-distribution -fno-semantic-interposition -fipa-pta -fno-plt -fp-model fast" \
         -Dc_link_args="-flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
         -Dcpp_args="-finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -fgraphite-identity -floop-nest-optimize -ftree-loop-distribution -fno-semantic-interposition -fipa-pta -fno-plt -fp-model fast" \
