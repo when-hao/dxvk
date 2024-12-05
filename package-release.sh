b4ab6b0e10c02bc7cf21b0312f6057ac743ece90
when-hao <72253870+when-hao@users.noreply.github.com>
2024-12-05 22:42:00 +0800

    Update package-release.sh

--- package-release.sh
+++ package-release.sh
@@ -65,7 +65,11 @@ function build_arch {
   if [ $opt_devbuild -eq 0 ]; then
     opt_strip=--strip
   fi
-  
+  #-Dc_args="-fno-pic -fno-pie -finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -fgraphite-identity -floop-nest-optimize -ftree-loop-distribution -fno-semantic-interposition -fipa-pta -fno-plt -ffast-math -ffp-contract=fast -freciprocal-math -ffinite-math-only" \
+   #     -Dc_link_args="-flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
+    #    -Dcpp_args="-std=c++17 -fno-pic -fno-pie -finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -fgraphite-identity -floop-nest-optimize -ftree-loop-distribution -fno-semantic-interposition -fipa-pta -fno-plt -ffast-math -ffp-contract=fast -freciprocal-math -ffinite-math-only" \
+     #   -Dcpp_link_args="-flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
+        
   
   GCOV=x86_64-w64-mingw32-gcov meson setup --cross-file "$DXVK_SRC_DIR/$crossfile$1.txt" \
         --buildtype "release"                               \
@@ -75,9 +79,9 @@ function build_arch {
         --libdir "x$1" \
         -Db_pgo=off \
         -Db_sanitize=none \
-        -Dc_args="-fno-pic -fno-pie -finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -fgraphite-identity -floop-nest-optimize -ftree-loop-distribution -fno-semantic-interposition -fipa-pta -fno-plt -ffast-math -ffp-contract=fast -freciprocal-math -ffinite-math-only" \
+        -Dc_args="-fno-pic -fno-pie -finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -mllvm -polly -fno-semantic-interposition -fno-plt -ffast-math -ffp-contract=fast -freciprocal-math -ffinite-math-only" \
         -Dc_link_args="-flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
-        -Dcpp_args="-std=c++17 -fno-pic -fno-pie -finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -fgraphite-identity -floop-nest-optimize -ftree-loop-distribution -fno-semantic-interposition -fipa-pta -fno-plt -ffast-math -ffp-contract=fast -freciprocal-math -ffinite-math-only" \
+        -Dcpp_args="-fno-pic -fno-pie -finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -mllvm -polly -fno-semantic-interposition -fno-plt -ffast-math -ffp-contract=fast -freciprocal-math -ffinite-math-only" \
         -Dcpp_link_args="-flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
         -Db_ndebug=if-release                               \
         -Dbuild_id=$opt_buildid                             \
