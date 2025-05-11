082a77c797e8cc849e988e3bafa65b4ce918c595
when-hao <72253870+when-hao@users.noreply.github.com>
2025-05-11 15:32:08 +0800

    Update package-release.sh

--- package-release.sh
+++ package-release.sh
@@ -80,9 +80,9 @@ function build_arch {
         -Db_pgo=off \
         -Db_sanitize=none \
         -Dc_args="-include /usr/include/wine/wine/windows/d3d11on12.h -fno-pic -fno-pie -finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -fgraphite-identity -floop-nest-optimize -ftree-loop-distribution -fno-semantic-interposition -fipa-pta -fno-plt -ffast-math -ffp-contract=fast -freciprocal-math -ffinite-math-only" \
-        -Dc_link_args="-flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
+        -Dc_link_args="/usr/lib/gcc/x86_64-w64-mingw32/13-win32/libgcov.a -flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
         -Dcpp_args="-include /usr/include/wine/wine/windows/d3d11on12.h -fno-pic -fno-pie -finline-functions -fomit-frame-pointer -fno-stack-protector -fno-math-errno -fno-trapping-math -fno-common -fgraphite-identity -floop-nest-optimize -ftree-loop-distribution -fno-semantic-interposition -fipa-pta -fno-plt -ffast-math -ffp-contract=fast -freciprocal-math -ffinite-math-only" \
-        -Dcpp_link_args="-flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
+        -Dcpp_link_args="/usr/lib/gcc/x86_64-w64-mingw32/13-win32/libgcov.a -flto=full -s -fdata-sections -ffunction-sections -Wl,--gc-sections" \
         -Db_ndebug=if-release                               \
         -Dbuild_id=$opt_buildid                             \
         "$DXVK_BUILD_DIR/build.$1"
